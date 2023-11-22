bc147dc36f75a6389d5d4139783f4a2f2d2adbd2
unknown <md.tazwarul.islam.07@gmail.com>
2023-11-22 21:27:32 +0600

    fix banner text section

--- src/Components/HomePageComponent/Banner.jsx
+++ src/Components/HomePageComponent/Banner.jsx
@@ -12,7 +12,7 @@ const Banner = () => {
         <LazyLoad height={200} offset={100} once>
             <div className="  hero relative min-h-[60vh] md:min-h-[80vh] px-3  " style={{ backgroundImage: `url(${img2})` }}>
 
-                <div className="lg:absolute lg:left-[150px] lg:bottom-1/2 md:left-[100px] md:top-25 lg:top-30 text-center lg:text-left">
+                <div className="lg:absolute lg:left-[150px] lg:bottom-1/2 md:left-[100px] md:top-25 lg:top-28 text-center lg:text-left">
 
                     <h1 className="mb-5 text-5xl md:text-7xl  font-bold text-orange-400">Delicious Foods  With <br /> Wonderful Eating</h1>
                     <p className="mb-5 text-2xl font-semibold text-green-500">Satisfy Your Cravings</p>
